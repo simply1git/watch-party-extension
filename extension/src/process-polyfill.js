d346c6f7e5293e6097bf6f5ff916371d24910d90
simply1git <my1simply@gmail.com>
2026-02-03 02:44:46 +0530

    Fix CSP violation by moving inline polyfill to external file

--- extension/src/process-polyfill.js
+++ extension/src/process-polyfill.js
@@ -1,20 +1,22 @@
-// Shim for global process variable
-const processShim = {
-  env: {
-    NODE_ENV: 'production',
-    PLASMO_PUBLIC_SERVER_URL: 'https://watch-party-server-7tvy.onrender.com'
-  },
-  version: '',
-  platform: 'browser',
-  nextTick: (cb) => setTimeout(cb, 0),
-  cwd: () => '/'
-};
+// Shim for global process variable - Browser Compatible
+(function() {
+  const processShim = {
+    env: {
+      NODE_ENV: 'production',
+      PLASMO_PUBLIC_SERVER_URL: 'https://watch-party-server-7tvy.onrender.com'
+    },
+    version: '',
+    platform: 'browser',
+    nextTick: (cb) => setTimeout(cb, 0),
+    cwd: () => '/'
+  };
 
-// Expose on global object
-if (typeof globalThis !== 'undefined') {
-  globalThis.process = processShim;
-} else if (typeof window !== 'undefined') {
-  window.process = processShim;
-}
-
-module.exports = processShim;
+  // Expose on global object
+  if (typeof globalThis !== 'undefined') {
+    globalThis.process = processShim;
+  } else if (typeof window !== 'undefined') {
+    window.process = processShim;
+  }
+  
+  console.log('Process polyfill loaded manually');
+})();
