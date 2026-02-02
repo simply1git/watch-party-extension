1b2d02c1bb0df824477583cf09960007eae831a9
simply1git <my1simply@gmail.com>
2026-02-03 02:47:54 +0530

    Fix React is not defined error by importing React explicitly

--- extension/src/popup-content.tsx
+++ extension/src/popup-content.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { CONFIG } from "./config"
 
 console.log("Popup script executing...");
--- extension/src/popup.tsx
+++ extension/src/popup.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./polyfills";
 import PopupContent from "./popup-content";
 
